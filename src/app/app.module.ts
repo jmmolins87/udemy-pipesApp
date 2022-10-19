38652ed81843595096f11904b053471eb2d8b3f9
jmmcDesign <35576283+jmmolins87@users.noreply.github.com>
2022-10-19 18:34:48 +0200

    wip - menu

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,21 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+// ** My modules **
+import { PrimeNgModule } from './prime-ng/prime-ng.module';
+
 import { AppComponent } from './app.component';
+import { SharedModule } from './shared/shared.module';
+
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    PrimeNgModule,
+    SharedModule
   ],
   providers: [],
   bootstrap: [AppComponent]
