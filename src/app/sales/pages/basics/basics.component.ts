c143974ff4ff75616c2280a5b5823fbe1d038357
Juanma <x493165@gruposantander.es>
2022-10-20 12:06:27 +0200

    fixed prime flex and wip date pipe

--- src/app/sales/pages/basics/basics.component.ts
+++ src/app/sales/pages/basics/basics.component.ts
@@ -2,10 +2,15 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-basics',
-  templateUrl: './basics.component.html',
-  styleUrls: ['./basics.component.css']
+  templateUrl: './basics.component.html'
 })
 export class BasicsComponent implements OnInit {
+
+  public nameLower: string = 'juanma';
+  public nameUpper: string = 'JUANMA';
+  public completeName: string = 'JuanMa MoLiNs COrtéS'
+  public today: Date = new Date(); // Fecha de hoy
+
 
   constructor() { }
 
